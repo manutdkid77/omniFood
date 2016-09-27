27ec499c27616be003cc0d7718fa4447a12e5a1d
manutdkid77 <manutdkid77@gmail.com>
2016-09-27 18:59:12 +0530

    Added Sticky Navigation

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,4 +2,15 @@ $(document).ready(function(){
 	$('.nav-btn').on('click',function(){
 		$('nav').toggleClass('nav-btn-show');
 	});
+
+	$('#section-2').waypoint(function(direction){
+		if(direction == 'down'){
+			$('header').addClass('sticky');
+			$('.logo img').attr('src','resources/img/logo.png');
+		}
+		else{
+			$('header').removeClass('sticky');
+			$('.logo img').attr('src','resources/img/logo-white.png');	
+		}
+	});
 });
