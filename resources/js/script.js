e6553423695e96a462c6ca760a6e766ea96bc8a2
manutdkid77 <manutdkid77@gmail.com>
2016-09-27 19:53:36 +0530

    Added Animate.css style

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
+
+	//Mobile Navigation Hamburger
 	$('.nav-btn').on('click',function(){
 		$('nav').toggleClass('nav-btn-show');
 	});
 
+	//Sticky Navigation script
 	$('#section-2').waypoint(function(direction){
 		if(direction == 'down'){
 			$('header').addClass('sticky');
@@ -13,4 +16,21 @@ $(document).ready(function(){
 			$('.logo img').attr('src','resources/img/logo-white.png');	
 		}
 	});
+
+	//Animations script
+	$('#section-2 .features-wrapper').waypoint(function(direction){
+		$('#section-2 .features-wrapper').addClass('animated fadeIn');
+	},{ offset: '50%'});
+
+	$('#section-5 .features-wrapper').waypoint(function(direction){
+		$('#section-5 .features-wrapper').addClass('animated fadeIn');
+	},{offset : '50%'});
+
+	$('.app-wrapper').waypoint(function(direction){
+		$('.app-photo img').addClass('animated slideInUp');
+	},{offset : '50%'});
+
+	$('.price-wrapper').waypoint(function(direction){
+		$('.price-box:first-of-type').addClass('animated pulse');
+	},{offset : '50%'});
 });
